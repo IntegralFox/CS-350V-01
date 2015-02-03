aeb32f7c21670663fb27e15b1a1a880ad3f05386
IntegralFox <michael.welborn0775@gmail.com>
2015-02-02 21:55:51 -0600

    Add depth first python program to hw3

---
+++ hw3/depthFirst.py
@@ -0,0 +1,71 @@
+'''
+Description: Depth First Search to solve the knapsack problem.
+Author: Michael Welborn
+Date: 2015/2/1
+'''
+
+# Include the utilities in nodeManagement.py and the stack data structure deque
+from nodeManagement import *
+from collections import deque
+import sys
+
+
+def depthFirstSearch(root, maxWeight):
+	best = root
+	open = deque([root])
+
+	while len(open):
+		current = open.pop()
+
+		# Discard and skip an item combination if it exceeds the weight limit
+		if current.weight > maxWeight:
+			continue
+
+		# Set this node as best if it has a greater value than the current best
+		if current.value > best.value:
+			best = current
+
+		# Add children of the current node to the open set
+		open.extend(genChildrenFor(current))
+
+		# Current node is not placed in a closed set and is instead
+		# garbage collected for space efficiency
+
+	return best
+
+
+# The filename is the last argument in the argument list
+argc = len(sys.argv)
+filename = sys.argv[argc - 1]
+
+file = open(filename)  # Open the file for parsing
+maxWeight = int(file.readline())  # The first line is the max length
+
+# loop through the rest of the lines and parse the items
+items = []
+for line in file:
+	# Split on the first two spaces, separating the weight, value, and
+	# description
+	lineItem = line.strip().split(None, 2)
+
+	weight = int(lineItem[0])
+	value = int(lineItem[1])
+	description = lineItem[2]
+
+	items.append(Item(weight, value, description))
+
+# Create a root node with value 0, weight 0, composed of no items
+root = Node(0, 0, [], items)
+
+solution = depthFirstSearch(root, maxWeight)
+
+print 'Best combination of items in a sack that can hold {0:d}'.format(weight)
+print 'has a value of {0:d}.'.format(solution.value)
+print '\nThe sack contains the following:'
+print 'Item                      Weight     Value'
+for i in range(0, len(solution.itemsInSack)):
+	print '{0:25} {1:<10d} {2:d}'.format(
+		solution.itemsInSack[i].description,
+		solution.itemsInSack[i].weight,
+		solution.itemsInSack[i].value
+	)
