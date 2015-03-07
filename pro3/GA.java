6722a1eb59494dc6cf134aad29ad895792a51446
IntegralFox <michael.welborn0775@gmail.com>
2015-03-07 15:36:27 -0600

    Initial skeleton of GA program

---
+++ pro3/GA.java
@@ -0,0 +1,10 @@
+/* Genetic Algorithm class to contain the population during evolution */
+
+import java.util.PriorityQueue;
+import java.util.Vector;
+
+class GA {
+	/* Member Variables */
+	private PriorityQueue<Chromosome> population;
+	private Vector<Double> stockHistory;
+}
---
+++ pro3/StockAlgorithm.java
@@ -0,0 +1,7 @@
+/* Main wrapper to handle file inputs and status output */
+
+class StockAlgorithm {
+	public static void main() {
+		
+	}
+}
