7e50b3d0a453d30f43b4ebcf8f666e8c3ee4f8b3
IntegralFox <michael.welborn0775@gmail.com>
2015-03-07 20:15:27 -0600

    Change case of constants

--- pro3/Chromosome.java
+++ pro3/Chromosome.java
@@ -6,11 +6,13 @@ import java.util.ArrayList;
 class Chromosome {
 	/* PRNG to be used by all the chromosomes */
 	private static final Random prng = new Random();
-	private static final Double mutationProbability = 0.001;
-	private static final Integer numRules = 3;
-	private static final Integer numOperators = 2;
-	private static final char[] rules = { 'm', 's', 'e' };
-	private static final char[] operators = { '&', '|' };
+
+	/* "Constants" that define the behaviour of chromosomes */
+	private static final Double MUTATION_PROBABILITY = 0.001;
+	private static final Integer NUM_RULES = 3;
+	private static final Integer NUM_OPERATORS = 2;
+	private static final char[] RULES = { 'm', 's', 'e' };
+	private static final char[] OPERATORS = { '&', '|' };
 
 	/* Member variables */
 	public Double fitness;
@@ -29,9 +31,9 @@ class Chromosome {
 	/* Member Functions */
 	public void mutate() {
 		for (int i = 0; i < representation.length(); ++i) {
-			if (prng.nextDouble() > mutationProbability) continue; // Skip the Ith character with probability 0.999 to mutate with probability 0.001
-			if (i%5 == 0) representation.setCharAt(i, rules[prng.nextInt(numRules)]); // The Ith character is a rule if it is congruent to 0 mod 5
-			else if (i%5 == 4) representation.setCharAt(i, operators[prng.nextInt(numOperators)]); // The Ith character is an operator if it is congruent to 4 mod 5
+			if (prng.nextDouble() > MUTATION_PROBABILITY) continue; // Skip the Ith character with probability 0.999 to mutate with probability 0.001
+			if (i%5 == 0) representation.setCharAt(i, RULES[prng.nextInt(NUM_RULES)]); // The Ith character is a rule if it is congruent to 0 mod 5
+			else if (i%5 == 4) representation.setCharAt(i, OPERATORS[prng.nextInt(NUM_OPERATORS)]); // The Ith character is an operator if it is congruent to 4 mod 5
 			else representation.setCharAt(i, Character.forDigit(prng.nextInt(10), 10)); // Otherwise the Ith character is a number
 		}
 	}
@@ -47,8 +49,8 @@ class Chromosome {
 		/* Create a random chromosome by randomly choosing values for each
 		 * character in the chromosome's representation */
 		for (int j = 0; j < 3; ++j) {
-			r.append( operators[ prng.nextInt( numOperators ) ] );
-			r.append( rules[ prng.nextInt( numRules ) ] );
+			r.append( OPERATORS[ prng.nextInt( NUM_OPERATORS ) ] );
+			r.append( RULES[ prng.nextInt( NUM_RULES ) ] );
 			for (int i = 0; i < 3; ++i) r.append( prng.nextInt(10) );
 		}
 
