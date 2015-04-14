5578550469e9b0bdbbd32198737667871e2aebbb
IntegralFox <michael.welborn0775@gmail.com>
2015-04-14 16:21:13 -0500

    Add provided RL files

---
+++ moutain-car/krandom.h
@@ -0,0 +1,7 @@
+#ifndef _KRANDOM_H_
+#define _KRANDOM_H_
+long double ran3(int *idum);
+int choose_random_int_value (int range);
+long double choose_random_value();
+void initialize_random_number_generator(int val);
+#endif
