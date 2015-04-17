1be210a6dcefdca06e2c1948ecaaba35ec1cd364
IntegralFox <michael.welborn0775@gmail.com>
2015-04-16 19:43:19 -0500

    Add discretization of real values and e-greedy choice of actions

--- moutain-car/learningAgent.cc
+++ moutain-car/learningAgent.cc
@@ -10,9 +10,13 @@
 #define V_MIN -0.07
 #define V_MAX 0.07
 
+
+int discretize(double, double, double, int);
+int eGreedyChoose(double*, double);
+
 int main() {
 	int numEpisodes;
-	double epsilon;
+	double epsilon, verbosity;
 
 	std::cout << "Enter the number of episodes to train over: " << std::flush;
 	std::cin >> numEpisodes;
@@ -20,9 +24,47 @@ int main() {
 	std::cout << "Enter the value of epsilon to use in learning: " << std::flush;
 	std::cin >> epsilon;
 
+	std::cout << "Enter verbosity: " << std::flush;
+	std::cin >> verbosity;
+
+	// Initialize Q[x][v][a] arbitrarily
 	double Q[NUM_DISCRETIZATIONS][NUM_DISCRETIZATIONS][NUM_ACTIONS];
 	for (int x = 0; x < NUM_DISCRETIZATIONS; ++x)
 		for (int v = 0; v < NUM_DISCRETIZATIONS; ++v)
 			for (int a = 0; a < NUM_ACTIONS; ++a)
 				Q[x][v][a] = choose_random_value();
+
+	// Repeat for each episode
+	for (int episode = 0; episode < numEpisodes; ++episode) {
+		mcar simulator;
+
+		for (int step = 0; !simulator.reached_goal(); ++step) {
+			int v = discretize(simulator.curr_vel(), V_MIN, V_MAX, NUM_DISCRETIZATIONS);
+			int x = discretize(simulator.curr_pos(), X_MIN, X_MAX, NUM_DISCRETIZATIONS);
+			int action = eGreedyChoose(Q[x][v], epsilon);
+			simulator.update_position_velocity(static_cast<ACTION>(action));
+		}
+	}
+}
+
+int discretize(double value, double valueMin, double valueMax, int discretizations) {
+	double discSize = (valueMin - valueMax) / discretizations;
+	double valueRelative = value - valueMin;
+	return static_cast<int>(valueRelative / discSize);
+}
+
+int eGreedyChoose(double* state, double epsilon) {
+	if (choose_random_value() < epsilon) {
+		// Return a random action with epsilon chance
+		return choose_random_int_value(2);
+	} else {
+		// Return best Q action with 1-epsilon chance
+		if (state[0] > state[1] && state[0] > state[2]) {
+			return 0;
+		} else if (state[1] > state[2]) {
+			return 1;
+		} else {
+			return 2;
+		}
+	}
 }
--- moutain-car/mc.h
+++ moutain-car/mc.h
@@ -13,69 +13,68 @@
 /*                                                                    */
 /**********************************************************************/
 
-#define GRAVITY -0.0025  // acceleration due to gravity 
+#define GRAVITY -0.0025  // acceleration due to gravity
 
-#define GOAL 0.5  // above this value means goal reached 
+#define GOAL 0.5  // above this value means goal reached
 
 // car position is limited to the following range
-#include "ostream"
+#include <iostream>
 
-static double POS_RANGE[2] = {-1.2, GOAL}; 
+static double POS_RANGE[2] = {-1.2, GOAL};
 
 
 // car velocity is limited to the following range
 
-static double VEL_RANGE[2] =  {-0.07,0.07}; 
+static double VEL_RANGE[2] =  {-0.07,0.07};
 
 
 // allowable actions
 
 enum ACTION {coast, forward, backward};
 
-int run_trial(int trial); // function prototype 
+int run_trial(int trial); // function prototype
 
-std::ostream& operator << (std::ostream& out, ACTION a); 
+std::ostream& operator << (std::ostream& out, ACTION a);
 
 
 // define the main class
 
 class mcar {
 
-	public: 
-	
-		mcar();  // constructor function defines a random starting point 
-				 // within the allowable position and velocity 
-				 
-		mcar(double pos, double vel);  // second constructor function with specified pos and vel
-				 
-		friend double random_pos();  // generate random starting position 
-		
-		friend double random_vel();  // generate random starting velocity 
-		
-		double reward();  // return -1 if goal not reached, else 0		
-		
-		double curr_pos(){ return(p); };  // retrieve current position
-		
-		double	curr_vel(){ return(v); };   // retrieve current velocity
-		
-		void set_curr_pos(double pos) { p = pos;};  // set position to pos
-		
-		void set_curr_vel(double vel) {v = vel;};  // set velocity to vel
-		
-		ACTION int_to_act(int action);  // return named act 
-		
-		ACTION choose_random_act();  // choose an action 
-		
-		void  update_position_velocity(ACTION a); // update velocity and velocity 
-		
-		int reached_goal();  // 1 if reached, else 0
-		
-		friend std::ostream& operator << (std::ostream& out, mcar c); // display position and velocity 
-		
-		
-	private: 
-	
-		double v, p; // current state = velocity and position 
-		
-}; 
+	public:
 
+		mcar();  // constructor function defines a random starting point
+				 // within the allowable position and velocity
+
+		mcar(double pos, double vel);  // second constructor function with specified pos and vel
+
+		friend double random_pos();  // generate random starting position
+
+		friend double random_vel();  // generate random starting velocity
+
+		double reward();  // return -1 if goal not reached, else 0
+
+		double curr_pos(){ return(p); };  // retrieve current position
+
+		double	curr_vel(){ return(v); };   // retrieve current velocity
+
+		void set_curr_pos(double pos) { p = pos;};  // set position to pos
+
+		void set_curr_vel(double vel) {v = vel;};  // set velocity to vel
+
+		ACTION int_to_act(int action);  // return named act
+
+		ACTION choose_random_act();  // choose an action
+
+		void  update_position_velocity(ACTION a); // update velocity and velocity
+
+		int reached_goal();  // 1 if reached, else 0
+
+		friend std::ostream& operator << (std::ostream& out, mcar c); // display position and velocity
+
+
+	private:
+
+		double v, p; // current state = velocity and position
+
+};
